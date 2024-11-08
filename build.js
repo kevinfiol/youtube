5d375f6322d03addd66136e8a50148d5983e8a7c
kevinfiol <me@kevinfiol.com>
2024-11-07 23:55:11 -0500

    Update build.js

--- build.js
+++ build.js
@@ -4,4 +4,4 @@ import { MODES } from './src/modes.js';
 const dev = process.argv.includes('-d');
 const write = process.argv.includes('-w');
 
-await render({ dev, write, mode: MODES.LIGHTTUBE });
+await render({ dev, write, mode: MODES.YOUTUBE });
